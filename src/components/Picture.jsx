7851836a9da7c04ca1cf701db4ce77294e541da2
ssimsir <s_simsir@yahoo.com>
2024-03-24 19:07:59 +0300

    like added

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
 import data from "../helper/data";
 
 export default function Picture() {
+
+  const [clickLike, setClickLike] = useState (false); 
+  const handleClickLike = (e) => {
+    setClickLike(!clickLike)
+  }
+
     return (
       <div className="pictureContainer">
         {data.map((item, index) => (
           <div key={index} className = "pictureMain">
             <div className="picture"><img src={item.src.large} alt={item.photographer} /></div>
-            
-            <h1>{item.photographer}</h1>
+            <div className="h1-like">
+              <h1>{item.photographer}</h1>
+              <div onClick={ (e) => handleClickLike(e)} className="like">
+                {
+                  clickLike?<i className="fa-solid fa-heart"></i>:<i className="fa-regular fa-heart"></i> 
+                }
+                
+                
+              </div>
+              
+
+              
+             
+            </div>
           </div>
         ))}
       </div>
